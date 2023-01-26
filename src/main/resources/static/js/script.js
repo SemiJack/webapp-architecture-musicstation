cce5d01a8eebddb68d5894f307790340c14f1d4f
SemiJack <jacek.pelczar.stud@pw.edu.pl>
2023-01-26 02:18:11 +0100

    hide inforamtion about user name if not logged in

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,4 +1,10 @@
 function successfullyLogout(){
     alert("You have been logged out");
 }
+document.addEventListener("DOMContentLoaded",function (){
+    if(document.getElementById('hello_message').innerText==="Jesteś zalogowany jako"){
+        document.getElementById('hello_message').style.display= 'none';
+    }
+});
+
 
